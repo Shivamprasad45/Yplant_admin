3afad0db677bf490c7f0106d6a3f9fa40d957e8b
Shivam gond <codewithharry35434@gmail.com>
2024-08-25 16:59:13 +0530

    Admin 11

--- src/app/api/Order/route.ts
+++ src/app/api/Order/route.ts
@@ -2,9 +2,10 @@ import DbConnect from "@/lib/mongodbconnect";
 
 import Order from "@/models/Checkout";
 import { NextRequest, NextResponse } from "next/server";
-DbConnect();
+
 export async function GET() {
   try {
+    await DbConnect();
     console.log("order");
     const All_order = await Order.find();
     return NextResponse.json(All_order);
